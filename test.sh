571e63cfd2481020f89a226fa29a9475f3d9ce61
Eric Karge <eric.karge@meteogroup.com>
2016-04-11 12:12:24 +0200

    test.sh: Accept additional parameters for `docker run`.

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ docker run -ti --rm -v "$PWD/test-files:/test-files:ro" \
     ${CONCOURSE_PASSWORD:+--env "CONCOURSE_PASSWORD=$CONCOURSE_PASSWORD"} \
     ${CONCOURSE_HOST:+--env "CONCOURSE_HOST=$CONCOURSE_HOST"} \
     ${CONCOURSE_PORT:+--env "CONCOURSE_PORT=$CONCOURSE_PORT"} \
-    busybox /test-files/setup.sh
+    "$@" busybox /test-files/setup.sh
