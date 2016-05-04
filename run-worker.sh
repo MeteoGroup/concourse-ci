3da07f033883d1304bd13a81af39bebe5864d174
Eric Karge <eric.karge@meteogroup.com>
2016-05-04 14:43:06 +0200

    docker-compose.yml: Added authentication options.

--- run-worker.sh
+++ run-worker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker run --rm -ti --privileged --entrypoint /concourse-worker.sh \
-    ${CONCOURSE_TSA_HOST:+--env CONCOURSE_TSA_HOST="${CONCOURSE_TSA_HOST}"} \
-    ${CONCOURSE_TSA_PORT:+--env CONCOURSE_TSA_PORT="${CONCOURSE_TSA_PORT}"} \
-    ${CONCOURSE_TSA_PUBKEY:+--env CONCOURSE_TSA_PUBKEY="${CONCOURSE_TSA_PUBKEY}"} \
-    ${CONCOURSE_WORKER_KEY:+--env CONCOURSE_WORKER_KEY="${CONCOURSE_WORKER_KEY}"} \
+    --env CONCOURSE_TSA_HOST \
+    --env CONCOURSE_TSA_PORT \
+    --env CONCOURSE_TSA_PUBKEY \
+    --env CONCOURSE_WORKER_KEY \
     "$@" "meteogroup/concourse-ci"
